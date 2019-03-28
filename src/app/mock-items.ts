9da544fe63c37e349e12eef7a2ba95510fba4c37
James Jiang <jamesjiang282@gmail.com>
2019-03-28 03:35:10 -0700

    initial commit

---
+++ src/app/mock-items.ts
@@ -0,0 +1,9 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [
+  { id: 1, name: 'Car Wash', desc: '' },
+  { id: 2, name: 'Laundry', desc: '' },
+  { id: 3, name: 'Vacuum', desc: '' },
+  { id: 4, name: 'Groceries', desc: '' },
+  { id: 5, name: 'Sleep', desc: '' },
+];
