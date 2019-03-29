6e174f2891f5e7f23ad060de08cca6b364029947
James Jiang <jamesjiang282@gmail.com>
2019-03-28 23:39:13 -0700

    Used localstorage to store input data.

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../item';
 import { ITEMS } from '../mock-items';
+import { LocalStorageService } from 'angular-2-local-storage';
+
 
 @Component({
   selector: 'app-toDoList',
@@ -11,11 +13,16 @@ import { ITEMS } from '../mock-items';
 export class ItemsComponent implements OnInit {
 
   items = ITEMS;
+  windowItems = [] as Item[];
   selectedItem: Item;
+
 
   constructor() { }
 
   ngOnInit() {
+    for(var i = 0; i < window.localStorage.length; i++) {
+      window.localStorage.getItem(String(window.localStorage.key(i)));
+    }
   }
 
   onSelect(item: Item): void {
--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Item } from '../item';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
   selector: 'app-viewList',
@@ -7,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ViewComponent implements OnInit {
+  message: string;
+  description: string;
+  keyNum = 0;
+  item: Item;
+  adding = false;
 
   constructor() { }
+
+  add(){
+    this.item = new Item;
+    this.item.id = this.keyNum++;
+    this.item.name = this.message;
+    this.item.desc = this.description;
+    window.localStorage.setItem(String(this.keyNum), JSON.stringify(this.item));
+    console.log(window.localStorage.getItem(1))
+    this.message = '';
+    this.description = '';
+  }
 
   ngOnInit() {
   }
